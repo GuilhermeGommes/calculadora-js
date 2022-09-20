a4a725b2730adf4ebe59bed86f21f919798ba7ed
Guilherme Silva <guilherk.dsilva@gmail.com>
2022-09-20 17:43:20 -0300

    making changes on scripts

--- script.js
+++ script.js
@@ -1,49 +1,19 @@
-var result = document.querySelector("#result") 
 
-function buttonClicked(val){
-    if (result.innerHTML.length < 12){
-        result.innerHTML += val
-    }
-    if(val == "AC"){
-        result.innerHTML = ""
-    }
- }
-window.addEventListener("keydown", function(e){
-    let valInput = ""
-    switch(e.key){
-        case "1":
-            valInput = "1"
-            break
-        case "2":
-            valInput = "2"
-            break
-        case "3":
-            valInput = "3"
-            break
-        case "4":
-            valInput = "4"
-            break
-        case "5":
-            valInput = "5"
-            break
-        case "6":
-            valInput = "6"
-            break   
-        case "7":
-            valInput = "7"
-            break   
-        case "8":
-            valInput = "8"
-            break
-        case "9":
-            valInput = "9"
-            break
-        case "0":
-            valInput = "0"
-            break
-    }
-    if (result.innerHTML.length < 12){
-        result.innerHTML += valInput
-    }
 
+const buttons = document.querySelectorAll('button')
+const displayPrevious = document.querySelector('#previousOperation')
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        displayPrevious.innerHTML += button.innerHTML
+
+        if (button.classList.contains("Func")){
+            switch(button.innerHTML){
+                case '+':
+                    console.log("Botao mais");
+                    break
+            }
+        }
+
+    })
 })
